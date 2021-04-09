ee664155cf3bf947be3cb34cf11976c383fdef7e
lee-tae-eon <mktears0609@gmail.com>
2021-04-10 01:22:20 +0900

    MVC & PUG

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ import express from "express";
 import morgan from "morgan"; // logging기능을 갖는 미들웨어 접속 정보나 status~~등등
 import helmet from "helmet"; // 보안 기능을 하는 미들웨어
 import cookieParser from "cookie-parser"; // cookie의 유저정보를 저장 for handle session
-import { userRouter } from "./router";
-
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express();
 
-const handleHome = (req, res) => res.send("hello people");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
+app.set("view engine", "pug");
 // middleware사용
 app.use(cookieParser());
 app.use(express.json());
@@ -18,12 +17,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-// route 설정.
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
 // use의 의미는 누군가 /user에 접속하면 userRouter를 전부 사용하겠다는 의미
 // /user에 접속하면 userRouter의 root에 접속하게 되고 user index를 보여준다.
-app.use("/user", userRouter);
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
--- router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import app from "./app";
-
-export const userRouter = express.Router();
-
-userRouter.get("/", (req, res) => res.send("user index"));
-userRouter.get("/edit", (req, res) => res.send("user edit"));
-userRouter.get("/password", (req, res) => res.send("user password"));
