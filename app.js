2e34ac0f2061db187c04673f42fce24f2b3003f7
lee-tae-eon <mktears0609@gmail.com>
2021-04-10 02:26:43 +0900

    pug & local middleware
    middleware for routes to templates

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ import express from "express";
 import morgan from "morgan"; // logging기능을 갖는 미들웨어 접속 정보나 status~~등등
 import helmet from "helmet"; // 보안 기능을 하는 미들웨어
 import cookieParser from "cookie-parser"; // cookie의 유저정보를 저장 for handle session
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 // middleware사용
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
 
-// use의 의미는 누군가 /user에 접속하면 userRouter를 전부 사용하겠다는 의미
-// /user에 접속하면 userRouter의 root에 접속하게 되고 user index를 보여준다.
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
