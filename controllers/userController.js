5b26cfe8aec5b3a13fb93529cf0803493abfea0f
lee-tae-eon <mktears0609@gmail.com>
2021-05-14 05:43:48 +0900

    update profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -130,7 +130,7 @@ export const userDetail = async (req, res) => {
   }
 };
 
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "EditProfile" });
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "changePassword" });
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,14 +3,14 @@ import routes from "../routes";
 import { onlyPrivate } from "../middlewares";
 import {
   changePassword,
-  editProfile,
+  getEditProfile,
   userDetail,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 // userRouter.get(routes.home, users);  /* app.js에 이미 라우터 설정이 되었기에 삭제 */
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
